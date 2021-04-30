e65a55a9844fe4e292b25fe31277f8930360280d
nguyepe2 <32175436+nguyepe2@users.noreply.github.com>
2021-04-30 11:26:31 -0700

    Update main.cpp
    Cleaning up the code

--- Q2 Prime-Number-Position/main.cpp
+++ Q2 Prime-Number-Position/main.cpp
@@ -3,28 +3,21 @@
 
 int primeNumber(int num) {
   
-  //std::cout << num;
-
   int pos = 0;
 
   // get the sequence of prime numbers before num
   for(int i = 1; i < num; i++) {
     int prime = 0;
-    //std::cout << "val of i: " << i << std::endl;
 
     for(int j = i-1; j > 1; j--) {
-      //std::cout << "val of j: " << j << std::endl;
-      //std::cout << "val of i % j: " << i % j << std::endl;
       if(i % j == 0) { // i is not a prime
         prime = 1;
-        //std::cout << "Prime is FALSE, breaking... " << std::endl;
         break;
       }
     }
-    //std::cout << "val of prime: " << prime << std::endl;
+
     if(prime == 0) {
       pos++;
-      //std::cout << "val of pos: " << pos << std::endl;
     }
   }
 
